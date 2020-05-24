aab767d4f8b85ce93088fd2e9ff43691244ceabb
lijingwen01 <ljw327981765>
2020-05-24 21:58:36 +0800

    源码

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/wenwen1613/blog-example/pkg/setting"
 	"net/http"
 )
 
@@ -9,9 +11,16 @@ func main() {
 	router := gin.Default()
 	router.GET("/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "lee",
+			"message": "SSS",
 		})
 	})
 
-	router.Run()
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
